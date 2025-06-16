cdcae8f757513a9fe273630282df0f79b48cb808
Shomari <nyusternie@sdot.io>
2025-06-16 14:10:32 -0400

    Update title.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight'
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Fapp Party 🎉 Docs',
+			title: 'FappParty 🎉 Docs',
 			social: {
 				github: 'https://github.com/nyusternie/miniapps-party',
 			},
